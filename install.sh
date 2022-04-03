97443287b4766290aeaccb3f601ffda1b6bf831b
james <29265756+ottter@users.noreply.github.com>
2022-04-03 00:48:26 -0400

    git config dots

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 for FILE in $(find . -type f -name ".*" ! -path ".git/*" ); do
-        rsync -aR --checksum $FILE ~ && source $FILE
+        rsync -aR --checksum $FILE ~ && source $FILE 2> /dev/null
         echo "Placing ${FILE:2}"
 done
 
